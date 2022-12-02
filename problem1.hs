72824b86fae8a676436f298bbc153bd78cfbb30a
farkon00 <sammer2016sammer@gmail.com>
2022-12-02 19:30:51 +0200

    Removed firstN from problem1.hs

--- problem1.hs
+++ problem1.hs
@@ -4,11 +4,5 @@ import Data.List (sort)
 problem1_1 :: [[Integer]] -> Integer
 problem1_1 = maximum . map sum
 
-firstN :: Integer -> [a] -> [a]
-firstN _ [] = []
-firstN _ [x] = [x]
-firstN 1 (x:xs) = [x]
-firstN n (x:xs) = x:firstN (n - 1) xs
-
 problem1_2 :: [[Integer]] -> Integer
-problem1_2 = sum . firstN 3 . reverse . sort . map sum
+problem1_2 = sum . take 3 . reverse . sort . map sum
